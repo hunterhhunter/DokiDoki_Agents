f824ac2b32a2496fd5cc2c9f129c6d54abf3919b
HLLT21 <nr8122@gmail.com>
2023-11-22 09:50:20 +0900

    - 페르소나 new day 판별 부분 추가.

--- main/backend/main.py
+++ main/backend/main.py
@@ -26,12 +26,22 @@ for p_name in _:
 # ----- 서버에서 반복
 for u_data in received:
     persona = personas[u_data.get_persona()]
-    persona.scratch.curr_time = datetime.datetime.strptime("February 14, 2023, 08:00:00", 
+    # ---- 페르소나 시간 관련 초기화 부분.
+    curr_time = datetime.datetime.strptime("February 14, 2023, 08:00:00", 
                                                 "%B %d, %Y, %H:%M:%S")
+    new_day = False
+    if not persona.scratch.curr_time: 
+      new_day = "First day"
+    elif (persona.scratch.curr_time.strftime('%A %B %d')
+          != curr_time.strftime('%A %B %d')):
+      new_day = "New day"
+    persona.scratch.curr_time = curr_time
+
+    # ----
     perceived = persona.perceive(u_data.get_object(), evc)
     retrieved = persona.retrieve(perceived)
     # planning = persona.plan(u_data.get_location(), personas, 'First day', retrieved)
-    planning = persona.plan(u_data.get_location(), personas, False, retrieved)
+    planning = persona.plan(u_data.get_location(), personas, new_day, retrieved)
     execution = persona.execute(planning)
 
     persona.save(_dir + u_data.get_persona() + "/bootstrap_memory")
