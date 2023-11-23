31b084b2e3967844a8a01a70a4f1b233b3e5bfda
Mac <mac@Macui-MacBookPro.local>
2023-11-23 18:02:36 +0900

    - frontend 디자인.

--- main/backend/unity_ipc.py
+++ main/backend/unity_ipc.py
@@ -1,11 +1,14 @@
+import requests
+import json
+
 class UN_Data():
     '''
     json으로 정보 교환.
     '''
-    def __init__(self, persona, loc, obj) -> None:
-        self.persona = persona
-        self.location = loc
-        self.object = obj
+    def __init__(self, json_data) -> None:
+        self.persona = json_data['persona']
+        self.location = json_data['location']
+        self.object = json_data['object']
 
     def get_location(self):
         return self.location
@@ -15,5 +18,37 @@ class UN_Data():
 
     def get_object(self):
         return self.object
-
     
+
+
+def request():
+    import requests
+
+    url = 'http://192.168.80.2:8000/text/process/'
+
+    params = {
+        'user_name' : 'Franz Alez',
+        'user_password' : 'Franz',
+        'user_text' : 'HelloWorld',
+        'user_id' : 123
+    }
+
+    res = requests.post(url, json=params)
+
+    print(res.json())
+
+def response():
+    import requests
+
+    url = 'http://192.168.80.2:8000/text/process/'
+
+    params = {
+        'user_name' : 'Franz Alez',
+        'user_password' : 'Franz',
+        'user_text' : 'HelloWorld',
+        'user_id' : 123
+    }
+
+    res = requests.post(url, json=params)
+
+    print(res.json())
